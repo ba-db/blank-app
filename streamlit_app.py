2a7814abe3b68e712d02fbaa70f2098687a8a88d
ba-db <britt11.deboer@gmail.com>
2025-10-20 14:13:17 +0000

    iconsbig

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,22 @@ from streamlit_navigation_bar import st_navbar
 import pages as pg
 
 
+HOUSE_ICON = """
+<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="rgb(49,51,63)">
+<path d="M5 19V9l7-5 7 5v10h-5v-6H10v6H5z"/>
+</svg>
+"""
+
+GRAPH_ICON = """
+<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="rgb(49,51,63)">
+<path d="M3 17h2v-7H3v7zm4 0h2V7H7v10zm4 0h2v-4h-2v4zm4 0h2V4h-2v13zm4 0h2V10h-2v7z"/>
+</svg>
+"""
+
+# --- Define pages with HTML icons above text ---
 pages = [
-    "🏠\nProperties",
-    "📈\nMore data"
+    f"<div class='nav-item'>{HOUSE_ICON}<div class='nav-label'>Properties</div></div>",
+    f"<div class='nav-item'>{GRAPH_ICON}<div class='nav-label'>More data</div></div>",
 ]
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -54,9 +67,11 @@ page = st_navbar(
 
 
 functions = {
-    "🏠\nProperties": pg.show_properties,
-    "📈\nMore data": pg.show_data,
+    pages[0]: pg.show_properties,
+    pages[1]: pg.show_data,
 }
+
+
 go_to = functions.get(page)
 if go_to:
     go_to()
