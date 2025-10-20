2adb8c4fb9af11c82549c88016be5dfb6392c100
ba-db <britt11.deboer@gmail.com>
2025-10-20 12:58:25 +0000

    test2

--- pages/properties.py
+++ pages/properties.py
@@ -2,17 +2,10 @@
 import streamlit as st
 
 
-def show_install():
-    st.header("Install")
+def show_properties():
+    st.header("Properties")
     st.write(
         """
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id diam 
-        pharetra, dapibus est fermentum, laoreet diam. Integer vitae consequat augue:
-
-        * Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        * Curabitur et ante euismod quam efficitur auctor dictum in elit.
-        * Donec id nunc rutrum, fringilla tellus sed, faucibus mi.
-        * Aenean vel augue sed augue gravida auctor quis in risus.
-        * Duis viverra leo iaculis lectus consequat, mollis tincidunt eros malesuada.
+        Here needs to be the information about the properties
         """
     )
--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,8 @@ from streamlit_navigation_bar import st_navbar
 import pages as pg
 
 
-st.set_page_config(initial_sidebar_state="collapsed")
-
-pages = ["Install", "User Guide", "API", "Examples", "Community", "GitHub"]
+pages = ["Properties", "More data"]
 parent_dir = os.path.dirname(os.path.abspath(__file__))
-logo_path = os.path.join(parent_dir, "cubes.svg")
-urls = {"GitHub": "https://github.com/gabrieltempass/streamlit-navigation-bar"}
 styles = {
     "nav": {
         "background-color": "royalblue",
@@ -34,23 +30,17 @@ styles = {
 }
 options = {
     "show_menu": False,
-    "show_sidebar": False,
 }
 
 page = st_navbar(
     pages,
-    urls=urls,
     styles=styles,
     options=options,
 )
 
 functions = {
-    "Home": pg.show_home,
-    "Install": pg.show_install,
-    "User Guide": pg.show_user_guide,
-    "API": pg.show_api,
-    "Examples": pg.show_examples,
-    "Community": pg.show_community,
+    "Properties": pg.show_properties,
+    "More data": pg.show_data,
 }
 go_to = functions.get(page)
 if go_to:
